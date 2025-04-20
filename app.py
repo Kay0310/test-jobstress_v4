06744b98c202f18a7df9f1793c4aa4dab8c2c3e7
Kay0310 <kangyoon.kim@ihealse.com>
2025-04-20 21:55:36 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="직무스트레스 설문 시스템", layout="centered")
+
+st.image("stress.png", use_column_width=False)
+
+st.title("직무스트레스 설문 시스템")
+st.write("안녕하세요. 본 설문은 직무스트레스 수준을 확인하기 위한 목적이며, 응답 내용은 익명으로 처리됩니다.")
